d64764c98c89f4dcd2a9fb21aff905f6e8f1c4af
Gordana <minjon56@gmail.com>
2018-05-20 17:21:42 +0200

    Initiated fullcalendar using fullcalendar js plugin

--- resources/views/appointments/create.blade.php
+++ resources/views/appointments/create.blade.php
@@ -2,19 +2,34 @@
 
 @section('title', '| New Appointment')
 
+@section('links')
+    <link rel="stylesheet" href="{{ asset('vendor/formvalidation/dist/css/formValidation.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/fullcalendar-3.9.0/fullcalendar.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/fullcalendar-3.9.0/fullcalendar.print.min.css') }}" type="media" >
+    <link rel="stylesheet" href="{{ asset('vendor/jquery-ui-1.12.1/jquery-ui.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/timepicker-1.6.3/timepicker-addon.css') }}">
+
+    <style>
+        .fc-view-container { background: #fff }
+    </style>
+@endsection
+
+@section('side')
+    <h4 class="text-uppercase mb-4 mt-1">Doctors</h4>
+    @foreach ($profiles as $profile)
+        <p>
+            <a href="#">
+                {{ $profile->name }}
+            </a>
+        </p>
+    @endforeach
+@endsection
+
 @section('content')
 
-    @php
-        $profile = \App\Profile::first();
-    @endphp
+    <div id="appointmentsCalendar"></div>
 
-    {{ $profile->name }}
-    @foreach ($profile->workdays as $wd)
-        <p>{{ $wd->name}} {{ $wd->pivot->start }}-{{ $wd->pivot->end }}</p>
-    @endforeach
-
-
-    <form action="{{ route('appointments.store') }}" method="POST">
+    {{-- <form action="{{ route('appointments.store') }}" method="POST">
 
         @csrf
 
@@ -90,5 +105,49 @@
         <div class="form-group">
             <button type="submit" class="btn btn-warning">Schedule appointment</button>
         </div>
-    </form>
+    </form> --}}
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('vendor/formvalidation/dist/js/formValidation.min.js') }}"></script>
+    <script src="{{ asset('vendor/formvalidation/dist/js/framework/bootstrap.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.1/moment.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.17/moment-timezone-with-data.min.js"></script>
+    {{-- <script src="{{ asset('vendor/moment-2.18.1/moment.min.js') }}"></script> --}}
+    {{-- <script src="{{ asset('vendor/moment-2.18.1/moment-timezone-with-data.min.js') }}"></script> --}}
+    <script src="{{ asset('vendor/fullcalendar-3.9.0/fullcalendar.js') }}"></script>
+    <script src="{{ asset('vendor/fullcalendar-3.9.0/gcal.js') }}"></script>
+    <script src="{{ asset('vendor/jquery-ui-1.12.1/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('vendor/timepicker-1.6.3/timepicker-addon.js') }}"></script>
+
+    <script>
+        var calendar = $('#appointmentsCalendar')
+
+        calendar.fullCalendar({
+            header: {
+                left: 'prev, next, today',
+                right: 'month, agendaWeek, agendaDay, list',
+                center: 'title',
+            },
+            handleWindowResize: true,
+            firstDay: 1,
+            editable: true,
+            selectable: true,
+            businessHours: [
+                {
+                    dow: [ 1, 2, 3, 4, 5 ],
+                    start: '09:00',
+                    end: '20:00'
+                },
+                {
+                    dow: [ 6 ],
+                    start: '10:00',
+                    end: '15:00'
+                }
+            ],
+            minTime: "09:00:00",
+            maxTime: "20:00:00",
+            eventLimit: true
+        })
+    </script>
 @endsection
